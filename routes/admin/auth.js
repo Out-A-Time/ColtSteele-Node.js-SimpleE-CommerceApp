7e51a20559c82f929326c1cac0a9eb3d86589566
Out-A-Time <101604946+Out-A-Time@users.noreply.github.com>
2022-09-16 12:10:21 +0100

    Section 30. Lecture 414 - done.

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const { check, validationResult } = require("express-validator");
+
+const { handleErrors } = require("./middlewares");
 const usersRepo = require("../../repositories/users");
 const signupTemplate = require("./../../views/admin/auth/signup");
 const signinTemplate = require("./../../views/admin/auth/signin");
@@ -41,14 +42,9 @@ router.post(
   "/signup",
   //Email validation by express-validation
   [requireEmail, requirePassword, requirePasswordConfirmation],
+  handleErrors(signupTemplate),
   async (req, res) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      return res.send(signupTemplate({ req, errors }));
-    }
-    console.log(errors);
-    const { email, password, passwordConfirmation } = req.body;
+    const { email, password } = req.body;
 
     //Create a user in our user repo to represent this person
     const user = await usersRepo.create({ email: email, password: password });
@@ -74,12 +70,8 @@ router.get("/signin", (req, res) => {
 router.post(
   "/signin",
   [requireEmailExists, requireValidPasswordForUser],
+  handleErrors(signinTemplate),
   async (req, res) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      return res.send(signinTemplate({ errors: errors }));
-    }
     const { email } = req.body;
 
     const user = await usersRepo.getOneBy({ email: email });
